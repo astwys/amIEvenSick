abd781efa59afe10f5408cebe6e7e836146dcb51
Michael Bartl <bar16091@spengergasse.at>
2017-05-23 14:31:45 +0200

    change sickness fetch to be made directly in the sicknesses component; added loading sicknesses component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import './Form.css'
 import { observer } from 'mobx-react'
 
-import { SymptomsCollection } from '../../stores/symptoms'
+// import { SymptomsCollection } from '../../stores/symptoms'
 
-require('es6-promise').polyfill();
-import fetch from 'isomorphic-fetch'
+// require('es6-promise').polyfill();
+// import fetch from 'isomorphic-fetch'
 
 import PropTypes from 'prop-types'
 
@@ -15,35 +15,12 @@ export default observer(class Form extends Component {
     }
 
 	onSubmit (event) {
-		let submitSymptoms = new SymptomsCollection()
 		event.preventDefault()
 		const { symptoms } = this.props
 
-		//let symptomsList = []
-		let sympt = null
-		symptoms.forEach(symp => {
-			//symptomsList.push(symp.attributes._data)
-			sympt = symp.attributes._data
-		})
-		//console.log(JSON.stringify(symptomsList))
-		console.log(JSON.stringify(sympt))
-
-		fetch('https://jsonplaceholder.typicode.com/todos', {
-			method: 'POST',
-			headers: {
-    			'Accept': 'application/json',
-    			'Content-Type': 'application/json'
-  			},
-  			// body: JSON.stringify(symptomsList)
-  			body: JSON.stringify(sympt)
-		})
-		.then(result => {return result.json()})
-		.then(output => submitSymptoms.add([output]))
-		.then(() => {
-			this.context.router.history.push({
- 				pathname: 'sicknesses',
- 				state: { symptoms: submitSymptoms }
-			})
+		this.context.router.history.push({
+			pathname: 'sicknesses',
+			state: { symptoms: symptoms }
 		})
 	}
 
--- src/components/Sicknesses/Sicknesses.js
+++ src/components/Sicknesses/Sicknesses.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import './Sicknesses.css'
+import { extendObservable } from 'mobx'
 import { observer } from 'mobx-react'
 //import sicknesses from '../../stores/sicknesses'
 import Sickness from '../Sickness/Sickness'
-//import Loading from '../Loading/Loading'
+import Loading from '../Loading/Loading'
+
+import { SymptomsCollection } from '../../stores/symptoms'
+
+require('es6-promise').polyfill();
+import fetch from 'isomorphic-fetch'
 
 import PropTypes from 'prop-types'
 
@@ -12,19 +18,53 @@ export default observer (class Sicknesses extends Component {
         router: PropTypes.object.isRequired
     }
 
+    constructor() {
+        super()
+
+        extendObservable(this, {
+            loading: true,
+            sicknesses: null
+        })
+    }
+
     componentWillMount() {
-        //sicknesses.fetch()
-        this.sicknesses = this.props.location.state.symptoms
-        console.log(this.props.location.state.symptoms.models)
+        let symptoms = this.props.location.state.symptoms
+        
+        console.log(this.props.location.state.symptoms)
+        this.fetchData(symptoms)
+    }
+
+    fetchData(symptoms) {
+        let submitSymptoms = new SymptomsCollection()
+
+        //let symptomsList = []
+        let sympt = null
+
+        symptoms.forEach(symp => {
+            // symptomsList.push(symp.attributes._data)
+            sympt = symp.attributes._data
+        })
+
+        fetch('https://jsonplaceholder.typicode.com/todos', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(sympt)
+        })
+        .then(result => { return result.json() })
+        .then(output => submitSymptoms.add([output]))
+        .then(() => {
+            this.sicknesses = submitSymptoms
+            this.loading = false
+        })
     }
 
     render() {
-        // const { sicknesses } = this.props.location.state.symptoms
-
-        // if (sicknesses.isRequest('fetching')) {
-        //     return <Loading label='sicknesses' />
-        // }
-
+        if (this.loading) {
+            return <Loading label='sicknesses' />
+        }
         return (
             <div className='Sicknesses'>
                 {
